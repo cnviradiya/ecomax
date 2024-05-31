be6404105d79fb8bfdfd5b8f8613c9565b5c392f
Trushang Rathod <trushang_rathod@itcoders.in>
2024-05-31 18:04:59 +0530

    products page des and respon Complit and detabase descriptionand sizes add complit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -82,15 +82,15 @@ $(document).ready(function () {
 
     var swiper = new Swiper('.Similar_Products', {
       slidesPerView: 1,
-      spaceBetween: 10,
+      spaceBetween: 30,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
       breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+        575: {
+          slidesPerView: 1,
+          spaceBetween: 30,
         },
         768: {
           slidesPerView: 2,

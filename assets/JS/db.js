09bd8ea6535698e82bce7fc7090a0f01ea900001
Trushang Rathod <trushang_rathod@itcoders.in>
2024-06-19 13:12:50 +0530

    deta entari

--- assets/JS/db.js
+++ assets/JS/db.js
@@ -29,7 +29,7 @@ const loadCollectionData = async (type="all", slug = null, collectionSlug = null
     return await fetchApi(type, slug, collectionSlug);
 }
 
-const industrial_url = "/assets/database/industrial_all_data.json";
+const industrial_url = "/assets/database/industrial.json";
 
 const industrialdata = async (type = "industrial_collection", slug , collectionSlug = null ) => {
     const allindustrial = await fetch(industrial_url).then((res) => res.json());

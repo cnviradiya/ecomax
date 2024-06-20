c3f118f8ed39cd18e1d0e141d35e63091c9c9812
Trushang Rathod <trushang_rathod@itcoders.in>
2024-06-20 15:08:51 +0530

    form data

--- api/index.php
+++ api/index.php
@@ -1,35 +1,56 @@
 <?php
-$to = "somebody@example.com, somebodyelse@example.com";
-$subject = "HTML email";
+$responseArr = [
+    'status' => 'FAILED',
+    'message' => 'Invalid data found'
+];
+if(!empty($_POST['email'])) {
+    try {
+        $to = $_POST['email'];
+        $subject = "HTML email";
+        
+        $message = "
+        <html>
+        <head>
+        <title>HTML email</title>
+        </head>
+        <body>
+        <p>Thank you for the contact us!</p>
+        <table>
+        <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        </tr>
+        <tr>
+        <td>John</td>
+        <td>Doe</td>
+        </tr>
+        </table>
+        </body>
+        </html>
+        ";
+        
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        
+        // More headers
+        $headers .= 'From: <info@ecomaxlubricant.com>' . "\r\n";
+        $headers .= 'Cc: trushang_rathod@itcoders.in' . "\r\n";
+        
+        mail($to,$subject,$message,$headers);
+        $responseArr = [
+            'status' => 'SUCCESS',
+            'message' => 'Thank you for reach us. we will get back soon.'
+        ];
+    } catch (\Throwable $th) {
+        //throw $th;
+        $responseArr['message'] = 'Something went wrong.';
+        $responseArr['error'] = $th->getMessage();
+    }
+    
+}
+echo json_encode($responseArr);
+exit;
 
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
 
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: <webmaster@example.com>' . "\r\n";
-$headers .= 'Cc: myboss@example.com' . "\r\n";
-
-mail($to,$subject,$message,$headers);
 ?>

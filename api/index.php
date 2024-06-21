a44bfeaac21a3f899ad8a0f9b25216a171059787
Trushang Rathod <trushang_rathod@itcoders.in>
2024-06-21 10:30:14 +0530

    data

--- api/index.php
+++ api/index.php
@@ -53,16 +53,14 @@ if(!empty($_POST['email'])) {
             fclose($handle);
 
             $encoded_content = chunk_split(base64_encode($content));
-        
+
             $body .= "--$boundary\r\n";
             $body .= "Content-Type: $type; name=\"$name\"\r\n";
             $body .= "Content-Disposition: attachment; filename=\"$name\"\r\n";
             $body .= "Content-Transfer-Encoding: base64\r\n\r\n";
             $body .= $encoded_content . "\r\n";
-           
-            // echo base64_dencode($encoded_content);
         }
-        
+
         $body .= "--$boundary--";
 
         if (mail($to, $subject, $body, $headers)) {

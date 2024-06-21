3bbbb2847ccdf9b95aeddfaca8fce745a61ed16c
Trushang Rathod <trushang_rathod@itcoders.in>
2024-06-21 17:47:19 +0530

    data

--- api/index.php
+++ api/index.php
@@ -13,7 +13,7 @@ if(!empty($_POST['email'])) {
         $reply_to_email = $from_email; //sender email, it will be used in "reply-to" header
         $subject  = "Sample mail for the career page"; 
         //subject for the email
-        $first_name = "Customer Name:-".$_POST["first_name"]; //body of the email
+        // $first_name = "Customer Name:-".$_POST["first_name"]; //body of the email
         // $last_name = $_POST["last_name"]; //body of the email
         // $customer_number = "Customer Name:-".$_POST["customernumber"]; //body of the email
         $message = "Message:-".$_POST["career_message"]; //body of the email

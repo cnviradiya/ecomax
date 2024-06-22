87f867fa8a1395e3dfb6ca94f3d0a028e5e2b8d0
Trushang Rathod <trushang_rathod@itcoders.in>
2024-06-22 09:32:59 +0530

    data

--- api/index.php
+++ api/index.php
@@ -16,7 +16,7 @@ if(!empty($_POST['email'])) {
         // $first_name = "Customer Name:-".$_POST["first_name"]; //body of the email
         // $last_name = $_POST["last_name"]; //body of the email
         // $customer_number = "Customer Name:-".$_POST["customernumber"]; //body of the email
-        $message = "Message:-".$_POST["career_message"]; //body of the email
+        $message = "Message:-".$_POST["Career_Message"]; //body of the email
     
         
         /*Always remember to validate the form fields like this
